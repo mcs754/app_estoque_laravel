d7aafa9066e5af8451b08bc22dfa4363725e1e04
Marcos Fernandes <mcs754@gmail.com>
2018-03-29 22:03:01 -0400

    Alteração final da aula 29/03/2018

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ProdutoController extends Controller
 {
     public function listar(){
-
-        $produtos = Produto::all();
-
-        foreach ($produtos as $produto){
-            echo $produto->descricao . "<br>";
-        }
+        //$produtos = Produto::all();
+        $p = Input::get('descricao'); //Só vai acontecer quando o post acontecer
+        $produtos = Produto::where('descricao', 'like', '%'.$p.'%')->get();
+        return view('produto.listar')->with('produtos', $produtos);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
        DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Sabão', 12, 3.5, '2018-11-01'));
        DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Amaciante', 20, 11, '2018-10-04'));
-       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Creme Dental', 100, 5, '2018-11-03'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Creme Dental', 100, 1, '2018-05-01'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Sabão Líquido', 50, 3, '2018-04-03'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Farinha', 70, 8, '2018-06-10'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Sabonete', 80, 11, '2018-11-09'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Óleo de soja', 30, 6, '2018-03-30'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Goiabada', 25, 7, '2018-07-08'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Arroz', 5, 8, '2018-11-03'));
+       DB::insert('insert into produtos (descricao, quantidade, valor, data_vencimento) values (?,?,?,?)', array ('Jiló', 99, 2, '2018-11-03'));
 
     }
 }
+
